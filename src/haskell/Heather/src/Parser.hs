778fbfb41424131c8f55d93d6edabb76b59bc9f9
Mads Gram <mgmadsgram@gmail.com>
2017-03-06 04:52:54 +0100

    Added scale and spaces

--- src/haskell/Heather/src/Parser.hs
+++ src/haskell/Heather/src/Parser.hs
@@ -7,10 +7,15 @@ import Text.ParserCombinators.Parsec.Combinator as ParSecCom
 import Text.Parsec.String (Parser)
 -- import Data.Char as Char
 import Data.Text as Text
+import Test.QuickCheck
 
 data Transfer = Transfer { tokenSymbol :: TokenSymbol,
                            to          :: Address,
                            from        :: Address
+} deriving Show
+
+data Scale = Scale { factor   :: Int,
+                     transfer :: Transfer
 } deriving Show
 
 type TokenSymbol = String
@@ -29,18 +34,36 @@ getAddress = do
     return $ prefix ++ addr
 
 transferFunction :: GenParser Char st Transfer
-transferFunction = do 
+transferFunction = do
     string "transfer"
     char '('
     ts <- getTokenSymbol
     char ','
+    spaces
     to <- getAddress
     char ','
+    spaces
     from <- getAddress
+    spaces
     char ')'
     return $ Transfer ts to from
 
+scaleFunction :: GenParser Char st Scale
+scaleFunction = do
+    string "scale"
+    char '('
+    factor <- read <$> many1 digit
+    char ','
+    spaces
+    transfer <- transferFunction
+    char ')'
+    return $ Scale factor transfer
 
 -- The end of line character is \n
 eol :: GenParser Char st Char
 eol = char '\n'
+
+--  TESTS!
+
+-- parse transferFunction "Error parse" "transfer(EUR, 0xffffffffffffffffffffffffffffffffffffffff, 0x0000000000000000000000000000000000000000)"
+-- parse scaleFunction "Error" "scale(10, transfer(EUR, 0xffffffffffffffffffffffffffffffffffffffff, 0x0000000000000000000000000000000000000000))"
