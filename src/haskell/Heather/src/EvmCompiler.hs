d5485c4eea7bd69666263a1065b8bccaf5a9ed4c
Thor <thorkilk@gmail.com>
2017-03-11 18:59:55 +0100

    Working on linker for EVM

--- src/haskell/Heather/src/EvmCompiler.hs
+++ src/haskell/Heather/src/EvmCompiler.hs
@@ -5,6 +5,7 @@ import IntermediateBahrLanguageDefinition
 import BahrParser
 import IntermediateCompiler
 
+import Numeric (showHex)
 import Text.Bytedump
 
 evmCompile :: IntermediateContract -> [EvmOpcode]
@@ -26,17 +27,56 @@ ppEvm instruction = case instruction of
     CALLVALUE -> "34"
     ISZERO    -> "15"
     PUSH1 w8  -> "60" ++ (hexString w8)
+    PUSH4 w32 -> "63" ++ (showHex w32 "")
     THROW     -> "fe"
     JUMPDEST  -> "5b"
     JUMPI     -> "57"
 
+getOpcodeSize :: EvmOpcode -> Integer
+getOpcodeSize (PUSH1  _)   = 2
+getOpcodeSize (PUSH2  _)   = 3
+getOpcodeSize (PUSH32 _)   = 33
+getOpcodeSize (JUMPITO _)  = 1 + 5 -- PUSH4 addr.; JUMPI
+getOpcodeSize (JUMPTO _)   = 1 + 5 -- PUSH4 addr.; JUMP
+getOpcodeSize (JUMPITOA _) = 1 + 5 -- PUSH4 addr.; JUMP
+getOpcodeSize (JUMPTOA _)  = 1 + 5 -- PUSH4 addr.; JUMP
+getOpcodeSize _            = 1
+
+replaceLabel :: Label -> Integer -> [EvmOpcode] -> [EvmOpcode]
+replaceLabel label int insts =
+  let
+    replaceLabelH label i inst = case inst of
+      (JUMPTO  l) -> if l==label then JUMPTOA  i else JUMPTO  l
+      (JUMPITO l) -> if l==label then JUMPITOA i else JUMPITO l
+      (JUMPDESTFROM l) -> if l==label then JUMPDEST else JUMPDESTFROM l
+      otherInst -> otherInst
+  in
+    map (replaceLabelH label int) insts 
+
+linker :: [EvmOpcode] -> [EvmOpcode]
+linker insts =
+  let
+    linkerH :: Integer -> [EvmOpcode] -> [EvmOpcode] -> [EvmOpcode]
+    linkerH inst_count insts_replaced (inst:insts) = case inst of
+      JUMPDESTFROM label -> linkerH (inst_count+1) (replaceLabel label inst_count insts_replaced) insts
+      _                  -> linkerH (inst_count + getOpcodeSize(inst)) insts_replaced insts
+    linkerH _ insts_replaced [] = insts_replaced
+  in
+    linkerH 1 insts insts
+
+eliminatePseudoInstructions :: [EvmOpcode] -> [EvmOpcode]
+eliminatePseudoInstructions (inst:insts) = case inst of
+  (JUMPTOA i)  -> (PUSH4 (fromInteger i)):JUMP:insts
+  (JUMPITOA i) -> (PUSH4 (fromInteger i)):JUMPI:insts
+  inst         -> inst:eliminatePseudoInstructions(insts)
+eliminatePseudoInstructions [] = []
+
 getContractHeader :: [EvmOpcode]
 getContractHeader =  [CALLVALUE,
                       ISZERO,
-                      PUSH1 6,
-                      JUMPI,
+                      JUMPITO "no_val0",
                       THROW,
-                      JUMPDEST,
+                      JUMPDESTFROM "no_val0",
                       STOP]
 
 getExecuteHeader = []
--- src/haskell/Heather/src/EvmLanguageDefinition.hs
+++ src/haskell/Heather/src/EvmLanguageDefinition.hs
@@ -4,6 +4,8 @@ import Data.Word
 
 type Word256 = (Word64, Word64, Word64, Word64)
 
+type Label = String
+
 data EvmOpcode = STOP
                | ADD
                | MUL
@@ -21,12 +23,18 @@ data EvmOpcode = STOP
                | MSTORE
                | SLOAD
                | SSTORE
-               | JUMP
+               | JUMPTO Label -- pseudo instruction: PUSH Addr(label); JUMP;
+               | JUMPITO Label -- pseudo instruction: PUSH Addr(label); JUMPI;
+               | JUMPTOA Integer -- 
+               | JUMPITOA Integer -- 
+               | JUMP -- Actual instruction
                | JUMPI
                | GAS
                | JUMPDEST
+               | JUMPDESTFROM Label
                | PUSH1 Word8
                | PUSH2 Word16
+               | PUSH4 Word32
                | PUSH32 Word256
                | DUP1
                | DUP2
