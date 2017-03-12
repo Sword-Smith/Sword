51d17992156a9206816bf118404a5efc01df98dd
Mads Gram <mgmadsgram@gmail.com>
2017-03-12 03:15:04 +0100

    Added hex rep for all opcodes (except push, dup, and swap) also added test for EvmCompiler

--- src/haskell/Heather/src/EvmCompiler.hs
+++ src/haskell/Heather/src/EvmCompiler.hs
@@ -24,20 +24,79 @@ asmToMachineCode opcodes = foldl (++) "" (map ppEvm opcodes)
 
 ppEvm :: EvmOpcode -> String
 ppEvm instruction = case instruction of
-    STOP      -> "00"
-    CALLVALUE -> "34"
-    ISZERO    -> "15"
-    PUSH1 w8  -> "60" ++ printf "%02x" w8
-    PUSH4 w32 -> "63" ++ printf "%08x" w32
-    THROW     -> "fe"
-    JUMPDEST  -> "5b"
-    JUMPI     -> "57"
+    STOP         -> "00"
+    ADD          -> "01"
+    MUL          -> "02"
+    SUB          -> "03"
+    DIV          -> "04"
+    SDIV         -> "05"
+    MOD          -> "06"
+    SMOD         -> "07"
+    ADDMOD       -> "08"
+    MULMOD       -> "09"
+    EXP          -> "0a"
+    SIGNEXTEND   -> "0b"
+    EVM_LT       -> "10"
+    EVM_GT       -> "11"
+    SLT          -> "12"
+    SGT          -> "13"
+    EVM_EQ       -> "14"
+    ISZERO       -> "15"
+    AND          -> "16"
+    OR           -> "17"
+    XOR          -> "18"
+    NOT          -> "19"
+    BYTE         -> "1a"
+    SHA3         -> "20"
+    ADDRESS      -> "30"
+    BALANCE      -> "31"
+    ORIGIN       -> "32"
+    CALLER       -> "33"
+    CALLVALUE    -> "34"
+    CALLDATALOAD -> "35"
+    CALLDATASIZE -> "36"
+    CALLDATACOPY -> "37"
+    CODESIZE     -> "38"
+    CODECOPY     -> "39"
+    GASPRICE     -> "3a"
+    EXTCODESIZE  -> "3b"
+    EXTCODECOPY  -> "3c"
+    BLOCKHASH    -> "40"
+    COINBASE     -> "41"
+    TIMESTAMP    -> "42"
+    NUMBER       -> "43"
+    DIFFICULTY   -> "44"
+    GASLIMIT     -> "45"
+    POP          -> "50"
+    MLOAD        -> "51"
+    MSTORE       -> "52"
+    MSTORES      -> "53"
+    SLOAD        -> "54"
+    SSTORE       -> "55"
+    JUMP         -> "56"
+    JUMPI        -> "57"
+    PC           -> "58"
+    MSIZE        -> "59"
+    GAS          -> "5a"
+    JUMPDEST     -> "5b"
+    PUSH1 w8     -> "60" ++ printf "%02x" w8
+    PUSH4 w32    -> "63" ++ printf "%08x" w32
+    DUP1         -> "80"
+    SWAP1        -> "90"
+    LOG0         -> "a0"
+    CREATE       -> "f0"
+    CALL         -> "f1"
+    CALLCODE     -> "f2"
+    RETURN       -> "f3"
+    DELEGATECALL -> "f4"
+    SUICIDE      -> "ff"
+    THROW        -> "fe"
+
 
 getOpcodeSize :: EvmOpcode -> Integer
 getOpcodeSize (PUSH1  _)   = 2
 getOpcodeSize (PUSH2  _)   = 3
-getOpcodeSize (PUSH4  _)   = 5
-getOpcodeSize (PUSH32 _)   = 33
+getOpcodeSize (PUSH4 _)    = 5
 getOpcodeSize (JUMPITO _)  = 1 + 5 -- PUSH4 addr.; JUMPI
 getOpcodeSize (JUMPTO _)   = 1 + 5 -- PUSH4 addr.; JUMP
 getOpcodeSize (JUMPITOA _) = 1 + 5 -- PUSH4 addr.; JUMP
@@ -68,8 +127,8 @@ linker insts =
 
 eliminatePseudoInstructions :: [EvmOpcode] -> [EvmOpcode]
 eliminatePseudoInstructions (inst:insts) = case inst of
-  (JUMPTOA i)  -> (PUSH4 (fromInteger i)):JUMP:insts
-  (JUMPITOA i) -> (PUSH4 (fromInteger i)):JUMPI:insts
+  (JUMPTOA i)  -> (PUSH4 (fromInteger i)):JUMP:eliminatePseudoInstructions(insts)
+  (JUMPITOA i) -> (PUSH4 (fromInteger i)):JUMPI:eliminatePseudoInstructions(insts)
   inst         -> inst:eliminatePseudoInstructions(insts)
 eliminatePseudoInstructions [] = []
 
@@ -90,7 +149,58 @@ getExecuteFooter = []
 test_EvmOpCodePush1Hex = PUSH1 0x60 :: EvmOpcode
 test_EvmOpCodePush1Dec = PUSH1 60 :: EvmOpcode
 
+-- ppEvm
+
 test_ppEvmWithHex = TestCase ( assertEqual "ppEvm with hex input" (ppEvm(test_EvmOpCodePush1Hex)) "6060" )
 test_ppEvmWithDec = TestCase ( assertEqual "ppEvm with dec input" (ppEvm(test_EvmOpCodePush1Dec)) "603c" )
 
-tests = TestList [TestLabel "test_ppEvmWithHex" test_ppEvmWithHex, TestLabel "test_ppEvmWithDec" test_ppEvmWithDec]
+-- getContractHeader
+
+test_getContractHeader = TestCase (assertEqual "getContractHeader test" (getContractHeader) ([CALLVALUE,ISZERO,JUMPITO "no_val0",THROW,JUMPDESTFROM "no_val0",STOP]))
+
+-- evmCompile
+
+exampleContact             = parse' "translate(100, both(scale(101, transfer(EUR, 0xffffffffffffffffffffffffffffffffffffffff, 0x0000000000000000000000000000000000000000)), scale(42, transfer(EUR, 0xffffffffffffffffffffffffffffffffffffffff, 0x0000000000000000000000000000000000000000))))"
+exampleIntermediateContact = intermediateCompile(exampleContact)
+
+test_evmCompile = TestCase( assertEqual "evmCompile test with two contracts" (evmCompile exampleIntermediateContact) (getContractHeader) )
+
+-- getOpcodeSize
+
+evm_opcode_push1       = PUSH1 0x60 :: EvmOpcode
+evm_opcode_push4       = PUSH4 0x60606060 :: EvmOpcode
+evm_opcode_pushJUMPITO = JUMPITO ":)" :: EvmOpcode
+evm_opcode_pushaADD    = ADD :: EvmOpcode
+
+test_getOpcodeSize_push1   = TestCase (assertEqual "test_getOpcodeSize_push1" (getOpcodeSize evm_opcode_push1) (2))
+test_getOpcodeSize_push4   = TestCase (assertEqual "test_getOpcodeSize_push4" (getOpcodeSize evm_opcode_push4) (5))
+test_getOpcodeSize_JUMPITO = TestCase (assertEqual "test_getOpcodeSize_JUMPITO" (getOpcodeSize evm_opcode_pushJUMPITO) (6))
+test_getOpcodeSize_ADD     = TestCase (assertEqual "evm_opcode_pushaADD" (getOpcodeSize evm_opcode_pushaADD) (1))
+
+-- linker
+
+exampleWithMultipleJumpDest = [JUMPITO "MADS",CALLVALUE,STOP,STOP,JUMPDESTFROM "MADS",ISZERO,JUMPITO "no_val0",THROW,JUMPDESTFROM "no_val0",STOP, JUMPTO "MADS", JUMPITO "MADS"]
+
+test_linker_mult_JumpDest = TestCase (assertEqual "test_linker_mult_JumpDest" (linker exampleWithMultipleJumpDest) ([JUMPITOA 10,CALLVALUE,STOP,STOP,JUMPDEST,ISZERO,JUMPITOA 19,THROW,JUMPDEST,STOP,JUMPTOA 10,JUMPITOA 10]))
+
+-- replaceLabel
+
+test_eliminatePseudoInstructions_mult_JumpDest = TestCase (assertEqual "test_eliminatePseudoInstructions_mult_JumpDest" (eliminatePseudoInstructions $ linker exampleWithMultipleJumpDest) ([PUSH4 10,JUMPI,CALLVALUE,STOP,STOP,JUMPDEST,ISZERO,PUSH4 19,JUMPI,THROW,JUMPDEST,STOP,PUSH4 10,JUMP,PUSH4 10,JUMPI]))
+
+-- asmToMachineCode
+
+test_asmToMachineCode_easy = TestCase (assertEqual "test_asmToMachineCode_easy" (asmToMachineCode $ eliminatePseudoInstructions $ linker [PUSH1 0x60, STOP, PC]) "60600058")
+test_asmToMachineCode_hard = TestCase (assertEqual "test_asmToMachineCode_hard" (asmToMachineCode $ eliminatePseudoInstructions $ linker exampleWithMultipleJumpDest) ("630000000a573400005b15630000001357fe5b00630000000a56630000000a57"))
+
+tests = TestList [TestLabel "test_ppEvmWithHex" test_ppEvmWithHex,
+                  TestLabel "test_ppEvmWithDec" test_ppEvmWithDec,
+                  TestLabel "test_getContractHeader" test_getContractHeader,
+                  TestLabel "test_evmCompile" test_evmCompile,
+                  TestLabel "test_getOpcodeSize_push1" test_getOpcodeSize_push1,
+                  TestLabel "test_getOpcodeSize_push4" test_getOpcodeSize_push4,
+                  TestLabel "test_getOpcodeSize_JUMPITO" test_getOpcodeSize_JUMPITO,
+                  TestLabel "test_getOpcodeSize_ADD" test_getOpcodeSize_ADD,
+                  TestLabel "test_linker_mult_JumpDest" test_linker_mult_JumpDest,
+                  TestLabel "test_eliminatePseudoInstructions_mult_JumpDest" test_eliminatePseudoInstructions_mult_JumpDest,
+                  TestLabel "test_asmToMachineCode_hard" test_asmToMachineCode_hard,
+                  TestLabel "test_asmToMachineCode_easy" test_asmToMachineCode_easy]
--- src/haskell/Heather/src/EvmLanguageDefinition.hs
+++ src/haskell/Heather/src/EvmLanguageDefinition.hs
@@ -9,38 +9,74 @@ type Label = String
 data EvmOpcode = STOP
                | ADD
                | MUL
+               | SUB
                | DIV
-               | EQ
+               | SDIV
+               | MOD
+               | SMOD
+               | ADDMOD
+               | MULMOD
+               | EXP
+               | SIGNEXTEND
+               | EVM_LT
+               | EVM_GT
+               | SLT
+               | SGT
+               | EVM_EQ
                | ISZERO
                | AND
+               | OR
+               | XOR
+               | NOT
+               | BYTE
+               | SHA3
+               | ADDRESS
+               | BALANCE
+               | ORIGIN
+               | CALLER
                | CALLVALUE
                | CALLDATALOAD
+               | CALLDATASIZE
+               | CALLDATACOPY
+               | CODESIZE
                | CODECOPY
+               | GASPRICE
                | EXTCODESIZE
+               | EXTCODECOPY
+               | BLOCKHASH
+               | COINBASE
                | TIMESTAMP
+               | NUMBER
+               | DIFFICULTY
+               | GASLIMIT
                | POP
                | MLOAD
                | MSTORE
+               | MSTORES
                | SLOAD
                | SSTORE
+               | JUMP
+               | JUMPI
                | JUMPTO Label -- pseudo instruction: PUSH Addr(label); JUMP;
                | JUMPITO Label -- pseudo instruction: PUSH Addr(label); JUMPI;
                | JUMPTOA Integer --
                | JUMPITOA Integer --
-               | JUMP -- Actual instruction
-               | JUMPI
+               | PC
+               | MSIZE
                | GAS
                | JUMPDEST
                | JUMPDESTFROM Label
                | PUSH1 Word8
-               | PUSH2 Word16
                | PUSH4 Word32
-               | PUSH32 Word256
                | DUP1
                | DUP2
                | SWAP1
                | SWAP2
+               | LOG0
+               | CREATE
                | CALL
+               | CALLCODE
                | RETURN
+               | DELEGATECALL
                | SUICIDE
-               | THROW deriving Show
+               | THROW deriving (Eq, Show)
