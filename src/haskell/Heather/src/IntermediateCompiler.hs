d90cb6809fc5cfb440142b9d919fcc7d19fe1ecb
Thor <thorkilk@gmail.com>
2017-03-24 01:20:30 +0100

    Fix error introduced in 862c5a08e373d47ce5c38bf222618604a8e176c5

--- src/haskell/Heather/src/IntermediateCompiler.hs
+++ src/haskell/Heather/src/IntermediateCompiler.hs
@@ -15,7 +15,7 @@ intermediateCompile :: Contract -> IntermediateContract
 intermediateCompile = IntermediateContract . getTransferCalls
 
 getTransferCalls :: Contract -> [TransferCall]
-getTransferCalls (Transfer sym to from) = [TransferCall 1 0 sym to from]
+getTransferCalls (Transfer sym from to) = [TransferCall 1 0 sym to from]
 getTransferCalls (Scale factor contract ) = map (scale factor) (getTransferCalls contract)
 getTransferCalls (Both contractA contractB) = getTransferCalls contractA ++ getTransferCalls contractB
 getTransferCalls (Translate time contract ) = map (translate time) (getTransferCalls contract)
