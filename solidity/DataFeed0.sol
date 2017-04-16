4429ca70e3cf9f83c96c65a70a185500f78ead34
Thor <thorkilk@gmail.com>
2017-04-16 15:52:48 +0200

    Make get method of DataFeed0 a constant method

--- solidity/DataFeed0.sol
+++ solidity/DataFeed0.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.9;
 
-contract DataFeed {
+contract DataFeed0 {
   uint256 price;
 
   function set(bytes32 _key, uint256 _price){
     price = _price;
   }
 
-  function get(bytes32 _key) returns (uint256 _price){
+  function get(bytes32 _key) constant returns (uint256 _price){
     return price;
   }
 }
