9e00d54acb3d9c6b13b3cf59249fb2a278383a39
Mads Gram <mgmadsgram@gmail.com>
2017-05-16 23:57:12 +0200

    Add the last Expressions

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -50,6 +50,87 @@ getType (DiviExp e0 e1) = do
     return IntType
     else
     Left $ "Error in division expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (LtExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if t0 == IntType && t1 == IntType then
+    return IntType
+  else
+    Left $ "Error in LtExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (GtExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if t0 == IntType && t1 == IntType then
+    return IntType
+  else
+    Left $ "Error in GtExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (EqExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if (t0 == IntType && t1 == IntType) then
+    return IntType
+  else
+    Left $ "Error in EqExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (GtOrEqExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if (t0 == IntType && t1 == IntType) then
+    return IntType
+  else
+    Left $ "Error in GtOrEqExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (LtOrEqExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if (t0 == IntType && t1 == IntType) then
+    return IntType
+  else
+    Left $ "Error in LtOrEqExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (OrExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if (t0 == BoolType && t1 == BoolType) then
+    return BoolType
+  else
+    Left $ "Error in OrExp expression! Expected bool, bool; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (AndExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if (t0 == BoolType && t1 == BoolType) then
+    return BoolType
+  else
+    Left $ "Error in AndExp expression! Expected bool, bool; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (MinExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if (t0 == IntType && t1 == IntType) then
+    return IntType
+  else
+    Left $ "Error in MinExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (MaxExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if (t0 == IntType && t1 == IntType) then
+    return IntType
+  else
+    Left $ "Error in MaxExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (NotExp e0) = do
+  t0 <- getType e0
+  if (t0 == BoolType) then
+    return BoolType
+  else
+    Left $ "Error in NotExp expression! Expected bool; got " ++ (show t0)
+getType (IfExp e0 e1 e2) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  t2 <- getType e2
+  if (t0 == BoolType && ((t1 == BoolType && t2 == BoolType) || (t1 == IntType && t2 == IntType))) then
+    return $ if t1 == BoolType then BoolType else IntType
+  else
+    if t0 != BoolType
+    then
+      Left $ "Error in IfExp expression! First exp must be of type bool; got " ++ show t0
+    else
+      Left $ "Error in IfExp expression! Types in both branches must match; got " ++ show t1 ++ ", " ++ show t2
 
 getLiteral :: Literal -> Either String ExpType
 getLiteral (IntVal _)                 = Right IntType
