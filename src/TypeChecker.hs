5c960101e36b5f962335a0b0f87f9647c3417f90
Thor <thorkilk@gmail.com>
2017-05-16 23:42:34 +0200

    Add type checking for first five expression types

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -17,22 +17,45 @@ import BahrParser
 
 -- checkExp :: Expression -> Either String Expression
 -- checkExp (Lit )
+
 getType :: Expression -> Either String ExpType
-getType (AndExp e0 e1) = do
-  t0 <- getType e0
-  t1 <- getType e1
-  if t0 == BoolType && t1 == BoolType then
-    do
-      return BoolType
-    else
-    Left $ "Error in and expression! Expected bool, bool; got " ++ (show t0) ++ ", " ++ (show t1)
 getType (Lit literal) = do
   t0 <- getLiteral literal
-  return (t0)
+  return t0
+getType (MultExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if t0 == IntType && t1 == IntType then
+    return IntType
+    else
+    Left $ "Error in multiplication expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (SubtExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if t0 == IntType && t1 == IntType then
+    return IntType
+    else
+    Left $ "Error in subtraction expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (AddiExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if t0 == IntType && t1 == IntType then
+    return IntType
+    else
+    Left $ "Error in addition expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+getType (DiviExp e0 e1) = do
+  t0 <- getType e0
+  t1 <- getType e1
+  if t0 == IntType && t1 == IntType then
+    return IntType
+    else
+    Left $ "Error in division expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
 
 getLiteral :: Literal -> Either String ExpType
-getLiteral (IntVal _)  = Right IntType
-getLiteral (BoolVal _) = Right BoolType
+getLiteral (IntVal _)                 = Right IntType
+getLiteral (BoolVal _)                = Right BoolType
+getLiteral (Observable OBool _ _ )    = Right BoolType
+getLiteral (Observable OInteger _ _ ) = Right IntType
 
 data ExpType = BoolType
              | IntType deriving (Show, Eq)
