1c917fa728652e6b7099a5ad017bfac5a03f667a
Mads Gram <mgmadsgram@gmail.com>
2017-05-18 21:56:33 +0200

    Remove unnecessary parenthesis in shows

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -42,97 +42,97 @@ getType (MultExp e0 e1) = do
   if t0 == IntType && t1 == IntType then
     return IntType
     else
-    Left $ "Error in multiplication expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in multiplication expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (SubtExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if t0 == IntType && t1 == IntType then
     return IntType
     else
-    Left $ "Error in subtraction expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in subtraction expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (AddiExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if t0 == IntType && t1 == IntType then
     return IntType
     else
-    Left $ "Error in addition expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in addition expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (DiviExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if t0 == IntType && t1 == IntType then
     return IntType
     else
-    Left $ "Error in division expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in division expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (LtExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if t0 == IntType && t1 == IntType then
     return BoolType
   else
-    Left $ "Error in LtExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in LtExp expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (GtExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if t0 == IntType && t1 == IntType then
     return BoolType
   else
-    Left $ "Error in GtExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in GtExp expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (EqExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if (t0 == IntType && t1 == IntType) then
     return BoolType
   else
-    Left $ "Error in EqExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in EqExp expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (GtOrEqExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if (t0 == IntType && t1 == IntType) then
     return BoolType
   else
-    Left $ "Error in GtOrEqExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in GtOrEqExp expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (LtOrEqExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if (t0 == IntType && t1 == IntType) then
     return BoolType
   else
-    Left $ "Error in LtOrEqExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in LtOrEqExp expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (OrExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if (t0 == BoolType && t1 == BoolType) then
     return BoolType
   else
-    Left $ "Error in OrExp expression! Expected bool, bool; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in OrExp expression! Expected bool, bool; got " ++ show t0 ++ ", " ++ show t1
 getType (AndExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if (t0 == BoolType && t1 == BoolType) then
     return BoolType
   else
-    Left $ "Error in AndExp expression! Expected bool, bool; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in AndExp expression! Expected bool, bool; got " ++ show t0 ++ ", " ++ show t1
 getType (MinExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if (t0 == IntType && t1 == IntType) then
     return IntType
   else
-    Left $ "Error in MinExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in MinExp expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (MaxExp e0 e1) = do
   t0 <- getType e0
   t1 <- getType e1
   if (t0 == IntType && t1 == IntType) then
     return IntType
   else
-    Left $ "Error in MaxExp expression! Expected int, int; got " ++ (show t0) ++ ", " ++ (show t1)
+    Left $ "Error in MaxExp expression! Expected int, int; got " ++ show t0 ++ ", " ++ show t1
 getType (NotExp e0) = do
   t0 <- getType e0
   if (t0 == BoolType) then
     return BoolType
   else
-    Left $ "Error in NotExp expression! Expected bool; got " ++ (show t0)
+    Left $ "Error in NotExp expression! Expected bool; got " ++ show t0
 getType (IfExp e0 e1 e2) = do
   t0 <- getType e0
   t1 <- getType e1
