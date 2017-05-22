6d7b1cb160125bfa35b6f26f61ce509cd16d16e1
Mads Gram <mgmadsgram@gmail.com>
2017-05-22 03:22:37 +0200

    Update the Eur_Option Solidity version to set addresses in constructor.

--- solidity/Eur_option.sol
+++ solidity/Eur_option.sol
@@ -8,12 +8,17 @@ contract Eur_Option {
     Tmc4 tmc;
     DataFeed0 dataFeed;
     bool contractExecuted; // default value is false
+    address to_address;
+    address from_address;
 
-    function Eur_Option()
+    function Eur_Option(address tmc_address, address datafeed_address,
+                        address to_address_, address from_address_)
     {
         deployTime = now;
-        tmc = Tmc4(0xf19a0bf79894b82cda0fc94b238b915d4fa35072); // Write in the correct address for the TMC
-        dataFeed = DataFeed0(0xb44c46bc94a2b4d2e1b363216eec996036d44281); // Write in the correct address for the DF
+        tmc = Tmc4(tmc_address); // Write in the correct address for the TMC
+        dataFeed = DataFeed0(datafeed_address); // Write in the correct address for the DF
+        to_address = to_address_;
+        from_address = from_address_;
     }
 
     function execute()
@@ -31,7 +36,7 @@ contract Eur_Option {
             uint obs_price = (obs_value - 10 >= 0) ? obs_value - 10 : 0; // Strike price is 10
             uint final_price = (obs_price <= 2000) ? obs_price : 2000; // 2000 is maxAmount from scale
 
-            tmc.transferFrom(0x2174c7f683d87012e8a1e4559be2f38bdc24b164, 0x2174c7f683d87012e8a1e4559be2f38bdc24b160, final_price);
+            tmc.transferFrom(from_address, to_address, final_price);
             contractExecuted = true;
         }
     }
