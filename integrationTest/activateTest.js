f08fa0d40d6156e7288d6d60d00b5873a30ab967
Mads Gram <mgmadsgram@gmail.com>
2018-04-06 01:23:27 +0200

    Add homemade assert functions and the balance checks needed.
    Still missing the return value checks.

--- integrationTest/activateTest.js
+++ integrationTest/activateTest.js
@@ -2,11 +2,29 @@ web3.eth.defaultAccount = web3.eth.accounts[0];
 me = web3.eth.defaultAccount;
 other = web3.eth.accounts[1];
 
-// TODO: Write assert
+function assertEquals(actual, expected, reason) {
+    if (actual !== expected) {
+        formatError(actual, expected, reason);
+    }
+}
+
+function assertNotEquals(actual, expected, reason) {
+    if (actual === expected) {
+        formatError(actual, expected, reason);
+    }
+}
+
+function formatError(actual, expected, reason) {
+        throw "\nActual: " + actual.toString() +
+              "\nExpected: " + expected.toString() +
+              (reason === "" ? "" : reason);
+}
+
+var dataFeedValue = 43;
 
 // Set data feed value
 console.log("Now setting data feed value to 43");
-var df = DataFeed0_.set( 0, 43, {from: me, gas: 3000000});
+var df = DataFeed0_.set( 0, dataFeedValue, {from: me, gas: 3000000});
 var t1 = web3.eth.getTransaction(df);
 while(t1.blockNumber === null){
     t1 = web3.eth.getTransaction(df);
@@ -60,9 +78,11 @@ while(t0_A.blockNumber === null){
 
 // TODO: Assert return value!
 
+var validApproveAmount = 100;
+
 // Approve with sufficient amount and activate
 // Confirm successful return value and that money has been moved to DC.
-b_A = Tmc4_DKK.approve( EPAddress, 100, {from: me, gas: 3000000} );
+b_A = Tmc4_DKK.approve( EPAddress, validApproveAmount, {from: me, gas: 3000000} );
 t0_A = web3.eth.getTransaction(b_A);
 while(t0_A.blockNumber === null){
     t0_A = web3.eth.getTransaction(b_A);
@@ -77,6 +97,7 @@ while(tcs.blockNumber === null){
 }
 // TODO: Assert return value
 
+var valueBeforeActivate = balanceOf(me);
 
 // Call to activate
 b_A = european_option_.activate({from: me, gas: 3000000});
@@ -86,7 +107,19 @@ while(t0_A.blockNumber === null){
 }
 
 // TODO: Assert return value
-// TODO: Assert balance changes
+
+// Assert that the money has been moved from me to DC
+// Check my balance change
+assertEquals(
+    Tmc4_DKK.balanceOf(me),
+    valueBeforeActivate - validApproveAmount,
+    "\nReason: Check my balance after activate");
+
+//Check DC balance change
+assertEquals(
+    Tmc4_DKK.balanceOf(EPAddress),
+    validApproveAmount,
+    "\nReason: Check DC balance after activate");
 
 // Execute the European option contract
 cs  = european_option_.execute({from: me, gas: 3000000});
@@ -96,17 +129,23 @@ while(tcs.blockNumber === null){
 }
 
 // TODO: Assert ret val
-// TODO: Assert balance changes
-// TODO: Assert that superfluous funds is sent back from DC
 
-// Check the balances of my own address and the address to which I have transferred
-// and check that the balance of DC is empty.
-console.log("********* AFTER EXECUTION OF EUROPEAN OPTION *********");
-balance_A = Tmc4_DKK.balanceOf(me);
-console.log("My balance on token contract DKK is: " + balance_A);
-balance_B = Tmc4_DKK.balanceOf(other);
-console.log("Other balance on token contract DKK is: " + balance_B);
+// Assert that the right amount was sent back to me after execute
+assertEquals(
+    Tmc4_DKK.balanceOf(me),
+    valueBeforeActivate - dataFeedValue,
+    "\nReason: Check my balance after execute");
 
+// Assert that DC has no money left
+assertEquals(
+    Tmc4_DKK.balanceOf(EPAddress),
+    0,
+    "\nReason: Check DC balance after execute");
+
+assertEquals(
+    Tmc4_DKK.balanceOf(other),
+    dataFeedValue,
+    "\nReason: Check other balance after execute");
 
 // Execute the DC again
 // Confirm failure and no balances was altered
