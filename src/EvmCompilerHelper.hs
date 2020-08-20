d6e420c14cb5d54df02e4b38eab2c898b55acdae
Ulrik-dk <ulrikelmelund@gmail.com>
2020-08-20 12:57:33 +0200

    remove trailing whitespace

--- src/EvmCompilerHelper.hs
+++ src/EvmCompilerHelper.hs
@@ -1,17 +1,17 @@
 -- MIT License
--- 
+--
 -- Copyright (c) 2019 Thorkil Værge and Mads Gram
--- 
+--
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
 -- in the Software without restriction, including without limitation the rights
 -- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 -- copies of the Software, and to permit persons to whom the Software is
 -- furnished to do so, subject to the following conditions:
--- 
+--
 -- The above copyright notice and this permission notice shall be included in all
 -- copies or substantial portions of the Software.
--- 
+--
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -287,10 +287,9 @@ solcSigSize     =   4
 byteSize        =   8
 
 -- Layout assuming 256bit args:
--- 
+--
 --              0x4  0x24 0x44
 -- [solcSigSize|arg0|arg1|arg2|..|argN]
 argNByteOffset n = solcSigSize + (n * wordSizeBits `div` byteSize)
-
 
 type FunDecl = String
